b8b500c584fbb43cce9ea467da8ec493de9d6ece
Thomas Leu <tomleu@gmail.com>
2016-04-29 12:43:38 -0500

    Add function for building single array from data

---
+++ bin/combineMLBData.js
@@ -0,0 +1,16 @@
+// import file system for reading/writing data
+var fs = require('fs');
+
+// base directory for data files
+var dir = '../data/';
+// get all filenames in data directory
+var dataFileNames = fs.readdirSync(dir);
+
+// read each file and concat into larger array
+var combinedData = [];
+dataFileNames.forEach(function(fileName) {
+  var games = JSON.parse(fs.readFileSync(dir+fileName, 'utf8'));
+  combinedData = combinedData.concat(games);
+});
+// write combined array to disk
+fs.writeFile(dir+'combinedMLBData.json', JSON.stringify(combinedData));
